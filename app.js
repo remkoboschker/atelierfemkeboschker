a35872dd3ccf05c3ea6898de0bb65c4a4bae7730
Remko Boschker <remko@informatietuin.nl>
2015-02-18 20:57:27 +0100

    link naar goudsmid-info op contact pagina

--- app.js
+++ app.js
@@ -196,7 +196,6 @@ module.exports = app;
  });
  */
 
-//http.createServer(app).listen(app.get('port'), function () {
+// http.createServer(app).listen(app.get('port'), function () {
 //    console.log("Express server listening on port " + app.get('port'));
-//});
-
+// });
